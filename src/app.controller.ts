08ca3e0c0d3e8be1f5300c1040161f59bb176c4b
alyzidan <Alyzidan.android@gmail.com>
2025-08-27 11:16:47 +0300

    initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get(':id')
+  getAdminName(@Param('id', ParseIntPipe) id: number): number {
+    console.log(id);
+    return id;
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
